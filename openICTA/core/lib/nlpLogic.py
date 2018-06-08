35ad3cd73401ecfdb877e3024c192a49818a3f47
Johan Louwers <louwersj@gmail.com>
2018-06-08 15:01:37 +0200

    add code and doc
    added some additional documentation and added nlpLogic.py

---
+++ openICTA/core/lib/nlpLogic.py
@@ -0,0 +1,63 @@
+# ----------------------------------------------------------------------------
+# name : nlpLogic.py
+#
+#
+# short discription:
+# nlpLogic forms the backbone for calling all NLP based operations on the text
+# provided to the openICTA. This will be primary focus (at this moment in time)
+# on providing a simplied binding towards spaCy for handling more complex 
+# operations and suport in returning less complex answers to the main code. As
+# from that point of view nlpLogic is more a binding solution between openICTA
+# and spaCy.
+#
+#
+# change log:
+# DATE         DEVELOPER                     REASON
+# 08JUN18      johan.louwers@capgemini.com   initial release to github
+# 08JUN18      johan.louwers@capgemini.com   creating a number of placeholders
+#                                            in the form of empty functions to
+#                                            ensure future development on them
+# 08JUN18      johan.lowuers@capgemini.com   release to github
+#
+# ----------------------------------------------------------------------------
+
+
+# import all needed libs 
+import spacy
+from spacy_cld import LanguageDetector
+
+# function languageList
+# function languageList will return all languages available within the solution
+# this is more of a helper function and can potentially be used for building a
+# gui solution at a later moment in time. Code should get the lLanguageList
+# values in a more dynamic (config) based manner and should not rely on the 
+# hardcoded values as done in the initial release.   
+def languageList():
+  lLanguageList = ['NL','US','DE']
+  print '[%s]' % ', '.join(map(str, lLanguageList))
+  return;
+
+
+
+
+# function languageDetect
+# function languageDetect is used to detect the language of the provided string
+# to ensure you can select the correct langauge options for additional NLP based
+# operations. It is possible to inlcude language detection in your logic directly
+# or you can make sure you us languageDetect first and provide the outcome as 
+# input directly to another logic function within the code. 
+def languageDetect( basetext ):
+  if type(basetext) != str:
+    print "ERROR; the value is not a string" 
+  else:
+    nlp = spacy.load('en')
+    language_detector = LanguageDetector()
+    nlp.add_pipe(language_detector)
+    doc = nlp(basetext)
+
+  return;
+
+languageDetect("abc");
+languageList();
+
+
--- openICTA/documentation/docs/v01/README.md
+++ openICTA/documentation/docs/v01/README.md
@@ -1,1 +1,8 @@
+Version 0.1 documentation
 
+
+# dependencies
+In case you run into an issue make sure you have all dependencies available on the system you try to run the code. Below is a list of all dependencies you will require to run all aspects of the openICTA
+
+* spaCy     : pip install -U spacy
+* spaCy-CLD : pip install spacy_cld
